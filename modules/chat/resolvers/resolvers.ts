58e77cd9be6a6424e1858b91a46bd6df36c78edd
Niccolò Belli <niccolo.belli@linuxsystems.it>
2019-01-29 10:11:02 +0100

    Step 7.15: Workaround subscriptions issue with React client

--- modules/chat/resolvers/resolvers.ts
+++ modules/chat/resolvers/resolvers.ts
@@ -2,6 +2,7 @@ import { PubSub, withFilter } from 'apollo-server-express';
 import { Chat } from '../../../entity/Chat';
 import { IResolvers } from '../../../types';
 import { ChatProvider } from '../providers/chat.provider';
+import { ModuleContext } from '@graphql-modules/core';
 
 export default {
   Query: {
@@ -27,15 +28,15 @@ export default {
   },
   Subscription: {
     chatAdded: {
-      subscribe: withFilter((root, args, { injector }) => injector.get(PubSub).asyncIterator('chatAdded'),
-        ({ chatAdded, creatorId }: { chatAdded: Chat, creatorId: number }, variables, { injector }) =>
-          injector.get(ChatProvider).filterChatAddedOrUpdated(chatAdded, creatorId)
+      subscribe: withFilter((root, args, { injector }: ModuleContext) => injector.get(PubSub).asyncIterator('chatAdded'),
+        (data: { chatAdded: Chat, creatorId: number }, variables, { injector }: ModuleContext) =>
+          data && injector.get(ChatProvider).filterChatAddedOrUpdated(data.chatAdded, data.creatorId)
       ),
     },
     chatUpdated: {
-      subscribe: withFilter((root, args, { injector }) => injector.get(PubSub).asyncIterator('chatUpdated'),
-        ({ chatUpdated, updaterId }: { chatUpdated: Chat, updaterId: number }, variables, { injector }) =>
-          injector.get(ChatProvider).filterChatAddedOrUpdated(chatUpdated, updaterId)
+      subscribe: withFilter((root, args, { injector }: ModuleContext) => injector.get(PubSub).asyncIterator('chatUpdated'),
+        (data: { chatUpdated: Chat, updaterId: number }, variables, { injector }: ModuleContext) =>
+          data && injector.get(ChatProvider).filterChatAddedOrUpdated(data.chatUpdated, data.updaterId)
       ),
     },
   },
